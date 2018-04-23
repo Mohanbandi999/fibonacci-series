debfc04921248e5219cf1116ff48cd89890934c6
Mohanbandi999 <mohanbandi999@gmail.com>
2018-04-23 20:06:10 +0530

    Add files via upload

---
+++ random(2 to6).py
@@ -0,0 +1,22 @@
+def rann(num):
+    
+    if num==2:
+        p=random.choice(range(11,99))
+        print(p)
+            
+    elif num==3:
+        c=random.choice(range(111,999))
+        print(c) 
+    
+    elif num==4:
+        d=random.choice(range(1111,9999))
+        print(d)
+        
+    elif num==5:
+        f=random.choice(range(11111,99999))
+        print(f)
+        
+    else:
+        g=random.choice(range(111111,999999))
+        print(g)
+    
